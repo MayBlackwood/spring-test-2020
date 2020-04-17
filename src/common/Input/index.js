3eaff9f5e716bc1d853f14c8efe90f2e52446d3f
MayBlackwood <ksenia.bobtsova@gmail.com>
2020-04-17 20:17:47 +0300

    code refactoring

--- src/common/Input/index.js
+++ src/common/Input/index.js
@@ -1,22 +1,19 @@
 import React from "react";
 import "./index.scss";
 
-class Input extends React.Component {
-  render() {
-    const { placeholder, value, name, handleChange } = this.props;
-    return (
-      <div className="input-wrapper">
-        <input
-          className="input"
-          type="text"
-          placeholder={placeholder}
-          value={value}
-          name={name}
-          onChange={handleChange}
-        />
-      </div>  
-    );
-  }
-}
+const Input = ({ placeholder, value, name, handleChange }) => {
+  return (
+    <div className="input-wrapper">
+      <input
+        className="input"
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        name={name}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 
 export default Input;
